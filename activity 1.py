1d63c6fc56446373ae089772174cf195c3016db6
habiburrahmanfakir99-pixel <habiburrahmanfakir99@gmail.com>
2026-03-26 18:47:43 +0200

    Add files via upload

---
+++ activity 1.py
@@ -0,0 +1,16 @@
+#Print a Sentence
+print("Welcome to the world of progamming!")
+
+
+#Print  N umber
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("Welcom to ", end= "*")
